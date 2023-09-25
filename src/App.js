600df2b1b44aa98193a0664f9545c57e69753f64
Harjyot Badh <badhHarjyot99@live.com>
2023-09-25 15:06:04 -0400

    Added React Router and Firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import NavBar from "./components/NavBar";
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <div>Hello There!</div>
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<NavBar />} />
+    {/* <Route path="/login" element={<Login />} />   If you are url /login -> This will bring you to the Login Page
+    <Route path="*" element={<ErrorPage />} />        If you are at any other URL -> Bring you to 404 Error*/}
+    <Route path="/test" element = {<h1>Test</h1>} />   {/* If you are at /test -> This will bring you to the Test Page */}
+  </Routes>;
+    
 }
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCluanebSV2sTW2CJ2Wza364A83JIvc3Vw",
+  authDomain: "gamequill-3bab8.firebaseapp.com",
+  projectId: "gamequill-3bab8",
+  storageBucket: "gamequill-3bab8.appspot.com",
+  messagingSenderId: "214027637857",
+  appId: "1:214027637857:web:f482142def73ff89684620",
+  measurementId: "G-JMY22R8ZLN"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
