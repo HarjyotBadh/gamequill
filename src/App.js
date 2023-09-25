630f5ce0f21af83168dad47ed1b64b2c56fa002d
bharrell1013 <bharrell1013@gmail.com>
2023-09-25 15:55:58 -0400

    Basic Profile Screen Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<NavBar />} />
-    {/* <Route path="/login" element={<Login />} />   If you are url /login -> This will bring you to the Login Page
+  return (
+    <Routes>
+      <Route path="/" element={<NavBar />} />
+      <Route path="/Profile" element={<Profile />} />
+      {/* <Route path="/login" element={<Login />} />   If you are url /login -> This will bring you to the Login Page
     <Route path="*" element={<ErrorPage />} />        If you are at any other URL -> Bring you to 404 Error*/}
-    <Route path="/test" element = {<h1>Test</h1>} />   {/* If you are at /test -> This will bring you to the Test Page */}
-  </Routes>;
-    
+      <Route path="/test" element={<h1>Test</h1>} />{" "}
+      {/* If you are at /test -> This will bring you to the Test Page */}
+    </Routes>
+  );
 }
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./NavBar";
+function Profile() {
+  return (
+    <div>
+      <NavBar />
+      This is a test
+    </div>
+  );
+}
+export default Profile;
