e74d52b991de038425b3b6bef7bf81d3ae1213a0
bharrell1013 <bharrell1013@gmail.com>
2023-10-25 12:26:45 -0400

    basic functionality for searching for user profiles

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { collection, getDoc, doc } from "firebase/firestore";
+import { Avatar } from "@material-tailwind/react";
+import "../styles/ProfileCard.css";
+
+const ProfileCard = ({ userId }) => {
+  const [userData, setUserData] = useState(null);
+  const fetchData = async () => {
+    //   const collection = collection(db, "profileData");
+    const userRef = doc(db, "profileData", userId);
+    const document = await getDoc(userRef);
+    if (document.exists) {
+      setUserData(document.data());
+    } else {
+      console.log("No such document!");
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [userId]);
+
+  return (
+    <div className="profile-card">
+      {userData && (
+        <>
+          <div className="profile-picture">
+            <Avatar
+              src={userData.profilePicture}
+              alt="Profile"
+              className="custom-avatar medium-avatar"
+            />
+          </div>
+          <h2>{userData.username}</h2>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProfileCard;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,42 +5,73 @@ import { Link } from "react-router-dom";
 import "../styles/SearchPage.css";
 import GameColumn from "../components/GamesColumn";
 import UserColumn from "../components/UserColumn";
+import { db } from "../firebase";
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
 
 const SearchPage = ({ searchQuery }) => {
   const [games, setGames] = useState([]);
   const [users, setUsers] = useState([]);
+  const searchGames = async () => {
+    try {
+      const corsAnywhereUrl = "http://localhost:8080/";
+      const apiUrl = "https://api.igdb.com/v4/games";
+
+      const response = await fetch(corsAnywhereUrl + apiUrl, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Client-ID": "71i4578sjzpxfnbzejtdx85rek70p6",
+          Authorization: "Bearer 7zs23d87qtkquji3ep0vl0tpo2hzkp",
+        },
+        body: `search "${searchQuery}";fields name, cover.url, aggregated_rating, involved_companies.company.name; limit:50; where category = (0,8,9);`,
+      });
+
+      const data = await response.json();
+      if (data.length) {
+        const gamesData = data.map((game) => ({
+          id: game.id,
+          gameData: game,
+        }));
+        setGames(gamesData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const searchUsers = async () => {
+    try {
+      const userResults = [];
+
+      const usersCollection = collection(db, "profileData");
+      const q = query(
+        usersCollection,
+        where("username", ">=", searchQuery),
+        where("username", "<=", searchQuery + "\uf8ff"),
+        limit(10)
+      );
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data();
+        userResults.push({
+          username: userData.username,
+          userId: doc.id,
+        });
+      });
+
+      setUsers(userResults);
+      // console.log(users[0].username);
+      console.log(users);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const searchGames = async () => {
-      try {
-        const corsAnywhereUrl = "http://localhost:8080/";
-        const apiUrl = "https://api.igdb.com/v4/games";
-
-        const response = await fetch(corsAnywhereUrl + apiUrl, {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Client-ID": "71i4578sjzpxfnbzejtdx85rek70p6",
-            Authorization: "Bearer 7zs23d87qtkquji3ep0vl0tpo2hzkp",
-          },
-          body: `search "${searchQuery}";fields name, cover.url, aggregated_rating, involved_companies.company.name; limit:50; where category = (0,8,9);`,
-        });
-
-        const data = await response.json();
-        if (data.length) {
-          const gamesData = data.map((game) => ({
-            id: game.id,
-            gameData: game,
-          }));
-          setGames(gamesData);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     if (searchQuery) {
       searchGames();
+      searchUsers();
     }
   }, [searchQuery]);
 
