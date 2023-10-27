cc83b4d7c24579672db7b2127447ac16f9902125
jackfurmanek <furmanek@purdue.edu>
2023-10-27 12:24:14 -0400

    First draft of Follow button

---
+++ src/components/FollowUser.js
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect } from 'react';
+// import gameLikedImage from '../images/buttons/gq-liked-shadow.png';
+// import gameNotLikedImage from '../images/buttons/gq-notliked-shadow.png';
+// import { db, auth } from "../firebase";
+// import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
+// export default function FollowUser() {
+//     return (
+//         <button onClick={handleButtonClick}>
+//             <img
+//                 src={isClicked ? gameLikedImage : gameNotLikedImage}
+//                 alt={isClicked ? 'Game liked!' : 'Game not liked.'}
+//             />
+//         </button>
+//     );
+// }
