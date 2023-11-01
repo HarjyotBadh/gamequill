8f385ffaf3336cbaa3da5745121857dd0cb012c0
bharrell1013 <bharrell1013@gmail.com>
2023-11-01 11:59:46 -0400

    css fixes

--- src/components/GameLike.js
+++ src/components/GameLike.js
@@ -1,65 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import gameLikedImage from '../images/buttons/gq-liked-shadow.png';
-import gameNotLikedImage from '../images/buttons/gq-notliked-shadow.png';
+import React, { useState, useEffect } from "react";
+import gameLikedImage from "../images/buttons/gq-liked-shadow.png";
+import gameNotLikedImage from "../images/buttons/gq-notliked-shadow.png";
 import { db, auth } from "../firebase";
-import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
-
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+} from "firebase/firestore";
+import "../styles/GameLikeLog.css";
 const GameLike = ({ gameID }) => {
-    const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const user = auth.currentUser.uid;
-                const docRef = doc(db, "profileData", user);
-                const docSnap = await getDoc(docRef);
-                const docLikes = docSnap.data().likes;
-
-                if (docLikes.includes(gameID)) {
-                    setIsClicked(true); // Set the button to 'on' state
-                }
-            } catch (error) {
-                console.error('Error fetching data from Firestore:', error);
-            }
-        };
-
-        fetchData();
-    }, [gameID]); // Add gameID as a dependency
-
-    // The code that runs every time the button is clicked
-    const handleButtonClick = async () => {
-        setIsClicked(prevState => !prevState);
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const user = auth.currentUser.uid;
         const docRef = doc(db, "profileData", user);
+        const docSnap = await getDoc(docRef);
+        const docLikes = docSnap.data().likes;
 
-        try {
-            if (isClicked) {
-                // Remove the gameID from the 'likes' array
-                await updateDoc(docRef, {
-                    likes: arrayRemove(gameID)
-                });
-                // console.log('Removed game ID from Like array');
-            } else {
-                // Add the gameID to the 'likes' array
-                await updateDoc(docRef, {
-                    likes: arrayUnion(gameID)
-                });
-                // console.log('Added game ID to Like array');
-            }
-        } catch (error) {
-            console.error('Error updating data in Firestore:', error);
+        if (docLikes.includes(gameID)) {
+          setIsClicked(true); // Set the button to 'on' state
         }
-    }
+      } catch (error) {
+        console.error("Error fetching data from Firestore:", error);
+      }
+    };
 
-    return (
-        <button onClick={handleButtonClick}>
-            <img
-                src={isClicked ? gameLikedImage : gameNotLikedImage}
-                alt={isClicked ? 'Game liked!' : 'Game not liked.'}
-            />
-        </button>
-    );
-}
+    fetchData();
+  }, [gameID]); // Add gameID as a dependency
+
+  // The code that runs every time the button is clicked
+  const handleButtonClick = async () => {
+    setIsClicked((prevState) => !prevState);
+
+    const user = auth.currentUser.uid;
+    const docRef = doc(db, "profileData", user);
+
+    try {
+      if (isClicked) {
+        // Remove the gameID from the 'likes' array
+        await updateDoc(docRef, {
+          likes: arrayRemove(gameID),
+        });
+        // console.log('Removed game ID from Like array');
+      } else {
+        // Add the gameID to the 'likes' array
+        await updateDoc(docRef, {
+          likes: arrayUnion(gameID),
+        });
+        // console.log('Added game ID to Like array');
+      }
+    } catch (error) {
+      console.error("Error updating data in Firestore:", error);
+    }
+  };
+
+  return (
+    <button className="gameLike" onClick={handleButtonClick}>
+      <img
+        src={isClicked ? gameLikedImage : gameNotLikedImage}
+        alt={isClicked ? "Game liked!" : "Game not liked."}
+      />
+    </button>
+  );
+};
 
 export default GameLike;
--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -6,71 +6,76 @@ import "../styles/TitleCard.css";
 import GameLog from "./GameLog";
 import GameLike from "./GameLike";
 import AddWishlistButton from "./AddWishlistButton";
-import { calculateAverageRating, generateStars } from "../functions/RatingFunctions";
+import {
+  calculateAverageRating,
+  generateStars,
+} from "../functions/RatingFunctions";
 
 export default function TitleCard({ gameData }) {
-    const [averageRating, setAverageRating] = React.useState(0);
-    const [darkMode, setDarkMode] = React.useState(
-        () =>
-            window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-    );
+  const [averageRating, setAverageRating] = React.useState(0);
+  const [darkMode, setDarkMode] = React.useState(
+    () =>
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
 
-    console.log("gameData: ", gameData);
+  console.log("gameData: ", gameData);
 
-    React.useEffect(() => {
-        const matcher = window.matchMedia("(prefers-color-scheme: dark)");
-        const onChange = (e) => setDarkMode(e.matches);
+  React.useEffect(() => {
+    const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+    const onChange = (e) => setDarkMode(e.matches);
 
-        matcher.addListener(onChange);
+    matcher.addListener(onChange);
 
-        return () => {
-            matcher.removeListener(onChange);
-        };
-    }, []);
+    return () => {
+      matcher.removeListener(onChange);
+    };
+  }, []);
 
-    React.useEffect(() => {
-        if (gameData.id) {
-            fetchReviewsByGameId(gameData.id).then((reviews) => {
-                setAverageRating(calculateAverageRating(reviews));
-            });
-        }
-    }, [gameData]);
+  React.useEffect(() => {
+    if (gameData.id) {
+      fetchReviewsByGameId(gameData.id).then((reviews) => {
+        setAverageRating(calculateAverageRating(reviews));
+      });
+    }
+  }, [gameData]);
 
-    if (!gameData) return <Spinner color="blue" />;
+  if (!gameData) return <Spinner color="blue" />;
 
-    const bigCoverUrl = gameData.cover
-        ? gameData.cover.url.replace("/t_thumb/", "/t_cover_big/")
-        : null;
-    const textSizeClass = gameData.name.length > 25 ? "text-xl" : "text-4xl";
+  const bigCoverUrl = gameData.cover
+    ? gameData.cover.url.replace("/t_thumb/", "/t_cover_big/")
+    : null;
+  const textSizeClass = gameData.name.length > 25 ? "text-xl" : "text-4xl";
 
-    const stars = generateStars(averageRating);
+  const stars = generateStars(averageRating);
 
-    return (
-        <div
-            className={`game-card ${darkMode ? "dark" : "light"}`}
-            data-theme={darkMode ? "dark" : "light"}
-        >
-            {bigCoverUrl && (
-                <Link to={`/game?game_id=${gameData.id}`}>  {/* Wrap the image in a Link */}
-                    <img src={bigCoverUrl} alt={gameData.name} />
-                </Link>
-            )}
-            <h2 className="title-card-text">{gameData.name}</h2>
-            <p>{gameData.involved_companies?.[0]?.company?.name || "N/A"}</p>
-            <p className="numericRating">{averageRating}</p>
-            <div className="rating">{stars}</div>
+  return (
+    <div
+      className={`game-card ${darkMode ? "dark" : "light"}`}
+      data-theme={darkMode ? "dark" : "light"}
+    >
+      {bigCoverUrl && (
+        <Link to={`/game?game_id=${gameData.id}`}>
+          {" "}
+          {/* Wrap the image in a Link */}
+          <img src={bigCoverUrl} alt={gameData.name} />
+        </Link>
+      )}
+      <h2 className="title-card-text">{gameData.name}</h2>
+      <p>{gameData.involved_companies?.[0]?.company?.name || "N/A"}</p>
+      <p className="numericRating">{averageRating}</p>
+      <div className="rating">{stars}</div>
 
-      <div class="play-buttons-container">
+      <div class="play-buttons-container flex flex-row">
         <div class="play-button">
           <GameLog gameID={gameData.id} />
         </div>
         <div class="play-button">
           <GameLike gameID={gameData.id} />
         </div>
-        <div class="play-button">
+      </div>
+      <div class="play-button">
         <AddWishlistButton gameID={gameData.id} />
-        </div>
       </div>
     </div>
   );
