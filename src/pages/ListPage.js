5b4e5ab9aebed6be4ca7d7101eeba767ffe37ff5
bharrell1013 <bharrell1013@gmail.com>
2023-11-06 16:02:18 -0500

    fix for listpage url

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const ListPage = () => {
-  const { listId } = useParams();
+  const { list_id } = useParams();
 
   // Fetch list data from Firebase using the listId
   // Display the list and its games
 
-  return <div>List Page for {listId}</div>;
+  return <div>List Page for {list_id}</div>;
 };
 
 export default ListPage;
