511558b7cb51549ce60aea25db0f90ed573cdfaf
bharrell1013 <bharrell1013@gmail.com>
2023-11-18 11:07:49 -0500

    Add drag and drop reminder styling

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -281,124 +281,132 @@ const ListPage = () => {
           )}
         </h1>
         {isUser && (
-          <div className="list-buttons-container flex flex-row">
-            <div className="toggle-button-container">
-              <button className="toggle-button" onClick={handleToggleListType}>
-                {listType === "ranked"
-                  ? "Switch to Unranked"
-                  : "Switch to Ranked"}
+          <div className="flex flex-col">
+            <div className="list-buttons-container flex flex-row">
+              <div className="toggle-button-container">
+                <button
+                  className="toggle-button"
+                  onClick={handleToggleListType}
+                >
+                  {listType === "ranked"
+                    ? "Switch to Unranked"
+                    : "Switch to Ranked"}
+                </button>
+              </div>
+              <button
+                className={`toggle-viewmode-button ${
+                  activeButton === "grid" ? "active" : ""
+                }`}
+                onClick={switchToGridView}
+                title="Grid View"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+                  />
+                </svg>
+              </button>
+              <button
+                className={`toggle-viewmode-button ${
+                  activeButton === "list" ? "active" : ""
+                }`}
+                onClick={switchToListView}
+                title="List View"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
+                  />
+                </svg>
+              </button>
+              <Popup
+                trigger={
+                  <button
+                    className={`featured-list-button ${
+                      isFeaturedList ? "active" : ""
+                    }`}
+                    title="Set as Featured List"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                      />
+                    </svg>
+                  </button>
+                }
+                modal
+                closeOnDocumentClick
+                contentStyle={{
+                  border: "2px solid white",
+                  height: 120,
+                  width: 400,
+                  padding: 20,
+                  backgroundColor: "grey",
+                }}
+              >
+                {(close) => (
+                  <div className="modal">
+                    <p>
+                      {isFeaturedList
+                        ? "Do you want to remove this as your featured list?"
+                        : "Do you want to set this as your featured list?"}
+                    </p>
+                    <button
+                      type="close"
+                      className="update-featured-button"
+                      onClick={() => {
+                        close();
+                        updateFeaturedList();
+                      }}
+                    >
+                      Yes
+                    </button>
+                    <button
+                      type="close"
+                      className="close-popup-button"
+                      onClick={() => {
+                        close();
+                      }}
+                    >
+                      No
+                    </button>
+                  </div>
+                )}
+              </Popup>
+              <button className="deleteListButton" onClick={handleDeleteList}>
+                Delete List
               </button>
             </div>
-            <button
-              className={`toggle-viewmode-button ${
-                activeButton === "grid" ? "active" : ""
-              }`}
-              onClick={switchToGridView}
-              title="Grid View"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
-                />
-              </svg>
-            </button>
-            <button
-              className={`toggle-viewmode-button ${
-                activeButton === "list" ? "active" : ""
-              }`}
-              onClick={switchToListView}
-              title="List View"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
-                />
-              </svg>
-            </button>
-            <Popup
-              trigger={
-                <button
-                  className={`featured-list-button ${
-                    isFeaturedList ? "active" : ""
-                  }`}
-                  title="Set as Featured List"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-6 h-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                    />
-                  </svg>
-                </button>
-              }
-              modal
-              closeOnDocumentClick
-              contentStyle={{
-                border: "2px solid white",
-                height: 120,
-                width: 400,
-                padding: 20,
-                backgroundColor: "grey",
-              }}
-            >
-              {(close) => (
-                <div className="modal">
-                  <p>
-                    {isFeaturedList
-                      ? "Do you want to remove this as your featured list?"
-                      : "Do you want to set this as your featured list?"}
-                  </p>
-                  <button
-                    type="close"
-                    className="update-featured-button"
-                    onClick={() => {
-                      close();
-                      updateFeaturedList();
-                    }}
-                  >
-                    Yes
-                  </button>
-                  <button
-                    type="close"
-                    className="close-popup-button"
-                    onClick={() => {
-                      close();
-                    }}
-                  >
-                    No
-                  </button>
-                </div>
-              )}
-            </Popup>
-            <button className="deleteListButton" onClick={handleDeleteList}>
-              Delete List
-            </button>
+            <div className="drag-and-drop-reminder">
+              Note: To change the order of the games in the list, drag and drop
+            </div>
           </div>
         )}
         {isUser && (
