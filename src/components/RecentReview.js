a706842487903af214cbaf274e65c91ceb25a010
HarjyotBadh <88450888+HarjyotBadh@users.noreply.github.com>
2023-11-28 18:08:47 -0500

    More UI Changes

--- src/components/RecentReview.js
+++ src/components/RecentReview.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/RecentReview.css";
+import Rating from "@mui/material/Rating";
 import tempcover from "../images/temp_images/tempcover.png";
 import { Link } from 'react-router-dom';
 import DOMPurify from 'dompurify';
 import { parseReviewWithSpoilersToHTML } from "../functions/ReviewFunctions";
-import { generateStars } from "../functions/RatingFunctions";
 
 export default function RecentReview({ cover, username, rating, note, id, reviewId, time }) {
     const date = time.toDate();
@@ -13,28 +13,11 @@ export default function RecentReview({ cover, username, rating, note, id, review
     if (!cover) {
         cover = tempcover;
     }
-
-    const [darkMode, setDarkMode] = useState(
-        () => window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
-    );
-
-    useEffect(() => {
-        const matcher = window.matchMedia("(prefers-color-scheme: dark)");
-        const onChange = (e) => setDarkMode(e.matches);
-
-        matcher.addListener(onChange);
-
-        return () => {
-            matcher.removeListener(onChange);
-        };
-    }, []);
-
     const displayText = note.length > 250 ? note.slice(0, 250) + '...' : note;
-    const stars = generateStars(rating);
 
     return (
         <div>
-            <div className={`rr-container ${darkMode ? "dark" : "light"}`} data-theme={darkMode ? "dark" : "light"}>
+            <div className={`rr-container`}>
                 <div className="rr-cover">
                     <Link to={`/game?game_id=${id}`}>
                         <img className="rr-cover-rounded" src={cover} alt="Game Cover" />
@@ -42,9 +25,21 @@ export default function RecentReview({ cover, username, rating, note, id, review
                 </div>
                 <div className="rr-text">
                     <h5 className="user-name-rating">
-                        {stars.map((star, index) => (
-                            <span key={index}>{star}</span>
-                        ))}
+                    {username} -
+                    <Rating
+                            name="read-only"
+                            value={rating}
+                            precision={0.5}
+                            readOnly
+                            sx={{
+                                "& .MuiRating-iconFilled": {
+                                    color: "var(--rating-color)",
+                                },
+                                "& .MuiRating-iconEmpty": {
+                                    color: "var(--star-color)",
+                                },
+                            }}
+                        />
                     </h5>
                     <Link to={`/review/${reviewId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                         <p
