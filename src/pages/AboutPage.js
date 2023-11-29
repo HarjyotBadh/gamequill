0eed85feb60e8bba62f6f3f713389d984d7960fa
HarjyotBadh <88450888+HarjyotBadh@users.noreply.github.com>
2023-11-29 12:46:43 -0500

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -11,24 +11,6 @@ export default function AboutPage() {
   const [gamesData, setGamesData] = useState([]);
   const game_ids = [142, 72, 127044, 40193];
 
-            <div class="about-grid">
-                <div class="about-meettheteam">
-                    MEET THE TEAM
-                </div>
-                <div class="about-meettheteam-about">
-                    The functionality of this website wouldn't be possible without the combined efforts of its four developers. Read about each of them here:
-                </div>
-
-                <div class="about-bio">
-                    
-                    <div class="about-text">
-                        <h4 class="about-name">JACK FURMANEK</h4>
-                        <p>Jack Furmanek is a Computer Science student at Purdue University, and also pursuing a minor in Mathematics. In his free time, he likes to play video games, watch TV shows, and build LEGO sets. Jack is interested in frontend programming, graphic design, and the user experience. He was responsible for the general layout of the home page, defining the site's visual style. He also designed some of the site's icons, including the ones for genres.</p>
-                    </div>
-                    <img class="about-img" src={tempcover} alt="what?" />
-                </div>
-
-            </div>
   useEffect(() => {
     (async () => {
       const fetchedGamesData = await fetchMultipleGameData(game_ids);
@@ -72,14 +54,7 @@ export default function AboutPage() {
           <div class="about-text">
             <h4 class="about-name">JACK FURMANEK</h4>
             <p>
-              "This is Jack's bio/testimonial. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-              occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum."
+              "Jack Furmanek is a Computer Science student at Purdue University, and also pursuing a minor in Mathematics. In his free time, he likes to play video games, watch TV shows, and build LEGO sets. Jack is interested in frontend programming, graphic design, and the user experience. He was responsible for the general layout of the home page, defining the site's visual style. He also designed some of the site's icons, including the ones for genres."
             </p>
           </div>
           <div className="about-img">
