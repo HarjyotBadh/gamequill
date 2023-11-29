0dfde0eaf7095fcda5c16ff9c78f46bafe1ddb2e
jackfurmanek <furmanek@purdue.edu>
2023-11-29 12:40:17 -0500

    Last push maybe?

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -16,13 +16,7 @@ export default function AboutPage() {
                 <div class="about-meettheteam-about">
                     The functionality of this website wouldn't be possible without the combined efforts of its four developers. Read about each of them here:
                 </div>
-                <div class="about-bio">
-                    <img class="about-img" src={tempcover} alt="what?" />
-                    <div class="about-text">
-                        <h4 class="about-name">HARJYOT BADH</h4>
-                        <p>"This is Harjyot's bio/testimonial. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                </div>
+
                 <div class="about-bio">
                     
                     <div class="about-text">
@@ -31,21 +25,7 @@ export default function AboutPage() {
                     </div>
                     <img class="about-img" src={tempcover} alt="what?" />
                 </div>
-                <div class="about-bio">
-                    <img class="about-img" src={tempcover} alt="what?" />
-                    <div class="about-text">
-                        <h4 class="about-name">BAILEY HARRELL</h4>
-                        <p>"This is Bailey's bio/testimonial. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                </div>
-                <div class="about-bio">
-                    
-                    <div class="about-text">
-                        <h4 class="about-name">AAYUSH NARAYANAN</h4>
-                        <p>"This is Aayush's bio/testimonial. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    <img class="about-img" src={tempcover} alt="what?" />
-                </div>
+
             </div>
 
             <Footer />
