9cb969c6a775781a93bf199d40e92c7f13986ce0
bharrell1013 <bharrell1013@gmail.com>
2023-12-01 11:28:37 -0500

    bug fix for css Featured1 and Featured2 components

--- src/components/Featured1.js
+++ src/components/Featured1.js
@@ -1,49 +1,47 @@
-import React, { useState } from 'react';
-import '../styles/Featured1.css';
+import React, { useState } from "react";
+import "../styles/Featured1.css";
 import tempscreenshot from "../images/temp_images/tempscreenshot.png";
-import { calculateAverageRating } from "../functions/RatingFunctions"
+import { calculateAverageRating } from "../functions/RatingFunctions";
 import { fetchReviewsByGameId } from "../functions/ReviewFunctions";
 
- 
 export default function Featured1({ gameData, screenshots }) {
-    const [averageRating, setAverageRating] = useState(null);
+  const [averageRating, setAverageRating] = useState(null);
 
-    React.useEffect(() => {
-        if (gameData.id) {
-          fetchReviewsByGameId(gameData.id).then((reviews) => {
-            setAverageRating(calculateAverageRating(reviews));
-          });
-        }
-      }, [gameData]);
-
-    if (!gameData) {
-        return (
-            <div class="image-cont rounded-corners">
-                <div class="image-container2">
-                    <img src={tempscreenshot} alt="Pikmin Test"/>
-                </div>
-            </div>
-        )
+  React.useEffect(() => {
+    if (gameData.id) {
+      fetchReviewsByGameId(gameData.id).then((reviews) => {
+        setAverageRating(calculateAverageRating(reviews));
+      });
     }
-    var imageUrl = screenshots[0];
-    var company = gameData.involved_companies[0].company.name;
-    var rating = averageRating;
-    
-    
-    
-    // Rating logic ends
+  }, [gameData]);
 
+  if (!gameData) {
     return (
-        
-        <div class="image-cont rounded-corners">
-            <div class="image-container2">
-                <img src={imageUrl} alt="Pikmin Test"/>
-            </div>
-            <div class="overlay"></div>
-            <div class="text-overlay">
-                <div class="game-name1">{gameData.name}</div>
-                <div class="developer1">{company} - {rating}</div>
-            </div>
+      <div class="image-cont rounded-corners">
+        <div class="image-container2">
+          <img src={tempscreenshot} alt="Pikmin Test" />
         </div>
+      </div>
     );
+  }
+  var imageUrl = screenshots[0];
+  var company = gameData.involved_companies[0].company.name;
+  var rating = averageRating;
+
+  // Rating logic ends
+
+  return (
+    <div class="image-cont rounded-corners">
+      <div class="image-container2">
+        <img src={imageUrl} alt="Pikmin Test" className="Featured1-image" />
+      </div>
+      <div class="overlay"></div>
+      <div class="text-overlay">
+        <div class="game-name1">{gameData.name}</div>
+        <div class="developer1">
+          {company} - {rating}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Featured2.js
+++ src/components/Featured2.js
@@ -6,50 +6,51 @@ import { calculateAverageRating } from "../functions/RatingFunctions";
 import { fetchReviewsByGameId } from "../functions/ReviewFunctions";
 
 export default function Featured1({ gameData, screenshots, limitSize }) {
-    const [averageRating, setAverageRating] = React.useState(null);
+  const [averageRating, setAverageRating] = React.useState(null);
 
-    React.useEffect(() => {
-        if (gameData.id) {
-            fetchReviewsByGameId(gameData.id).then((reviews) => {
-                setAverageRating(calculateAverageRating(reviews));
-            });
-        }
-    }, [gameData]);
-
-    if (!gameData) {
-        return (
-            <div class="image-cont rounded-corners">
-                <div class="image-container2">
-                    <img src={tempscreenshot} alt="Error Loading" />
-                </div>
-            </div>
-        );
+  React.useEffect(() => {
+    if (gameData.id) {
+      fetchReviewsByGameId(gameData.id).then((reviews) => {
+        setAverageRating(calculateAverageRating(reviews));
+      });
     }
+  }, [gameData]);
 
-    var imageUrl = screenshots && screenshots[0] ? screenshots[0] : defaultImage;
-    let company = "";
-    if (gameData.involved_companies && gameData.involved_companies.length > 0) {
-        company = gameData.involved_companies[0].company.name;
-    }
-
-    var rating = averageRating;
-
+  if (!gameData) {
     return (
-        <div class="image-cont rounded-corners">
-            <div class="image-container2">
-                <img
-                    src={imageUrl}
-                    alt="Error Loading"
-                    class={limitSize ? "limited-height" : ""}
-                />
-            </div>
-            <div class="overlay"></div>
-            <div class="text-overlay">
-                <div class="game-name2">{gameData.name}</div>
-                <div class="developer">
-                    {company} - {rating}
-                </div>
-            </div>
+      <div class="image-cont rounded-corners">
+        <div class="image-container2">
+          <img src={tempscreenshot} alt="Error Loading" />
         </div>
+      </div>
     );
+  }
+
+  var imageUrl = screenshots && screenshots[0] ? screenshots[0] : defaultImage;
+  let company = "";
+  if (gameData.involved_companies && gameData.involved_companies.length > 0) {
+    company = gameData.involved_companies[0].company.name;
+  }
+
+  var rating = averageRating;
+
+  return (
+    <div class="image-cont rounded-corners">
+      <div class="image-container2">
+        <img
+          src={imageUrl}
+          alt="Error Loading"
+          class={limitSize ? "limited-height" : ""}
+          className="Featured2-image"
+        />
+      </div>
+      <div class="overlay"></div>
+      <div class="text-overlay">
+        <div class="game-name2">{gameData.name}</div>
+        <div class="developer">
+          {company} - {rating}
+        </div>
+      </div>
+    </div>
+  );
 }
